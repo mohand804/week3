56f133e93883d704926f96a271a82a353c6c00ca
mohand804 <mohandabstar@gmail.com>
2025-10-09 05:44:58 +0300

    Add UserInfoBlocBuilder to VerifiedProfileRow for enhanced user information display.

--- lib/features/home/ui/widget/verified_profile_row.dart
+++ lib/features/home/ui/widget/verified_profile_row.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:week3/core/helpers/constant.dart';
 import 'package:week3/core/theming/styles.dart';
+import 'package:week3/features/home/ui/widget/user_info_bloc_builder.dart';
 
 class VerifiedProfileRow extends StatelessWidget {
   const VerifiedProfileRow({super.key});
@@ -11,6 +12,7 @@ class VerifiedProfileRow extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        UserInfoBlocBuilder(),
         Row(
           children: [
             Text(
